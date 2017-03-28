861a5957e407192e5b90c904407894119185b849
Richard Cobbe <rcobbe@rcobbe.net>
2017-03-28 14:47:46 -0400

    Add stub for unit tests

---
+++ test/RunTests.hs
@@ -0,0 +1,11 @@
+module Main(main) where
+
+import Control.Monad
+import System.Exit
+-- import Test.HUnit
+
+main :: IO ()
+main =
+  return ()
+  -- do c <- runTestTT tests
+  --   when (errors c /= 0 || failures c /= 0) exitFailure
