9091aeafa5f9d54871b08f93f98f8792c4aaf3b9
Richard Cobbe <rcobbe@rcobbe.net>
2017-04-13 20:59:40 -0400

    finish punct tests in Latin.ScannerTest

--- test/Latin/ScannerTest.hs
+++ test/Latin/ScannerTest.hs
@@ -15,7 +15,20 @@ tests = "Latin Scanner" ~:
 punctuationTests =
   "punctuation" ~:
   ["open paren" ~:
-   singleToken "(" ~?= Right (Tok_LParen (locn 1 1))]
+   singleToken "(" ~?= Right (Tok_LParen (locn 1 1)),
+
+   "close paren" ~:
+   singleToken ")" ~?= Right (Tok_RParen (locn 1 1)),
+
+   "ellipsis" ~:
+   singleToken "..." ~?= Right (Tok_Ellipsis (locn 1 1)),
+
+   "open paren with leading whitespace" ~:
+   singleToken "  (" ~?= Right (Tok_LParen (locn 1 3)),
+
+   "paren with line comment" ~:
+   tokenList "(; test comment\n)" ~?= Right [Tok_LParen (locn 1 1),
+                                             Tok_RParen (locn 2 1)]]
 
 stringTests =
   "string literals" ~: [3 ~?= 4]
@@ -31,6 +44,9 @@ numberTests =
 
 singleToken :: String -> Either String Token
 singleToken = scanSingleToken testSource
+
+tokenList :: String -> Either String [Token]
+tokenList = scan testSource
 
 testSource = "unit test input"
 
