6faf5e2e050d20d6e96e1ed38ac49d174b90cf2e
Richard Cobbe <rcobbe@rcobbe.net>
2017-07-16 14:45:46 -0700

    Switch LT.Latin.Output to use LT.Text instead of Data.Textual

--- lib/LT/Latin/Output.hs
+++ lib/LT/Latin/Output.hs
@@ -6,33 +6,33 @@ module LT.Latin.Output( letterToUnicode
 where
 
 import Prelude hiding (Word)
-import Data.Textual (Textual)
-import qualified Data.Textual as Textual
+import LT.Text (Text)
+import qualified LT.Text as Text
 
 import LT.Latin.Letter
 
 -- | Translates a 'Letter' to input syntax.
-letterToUnicode :: Textual a => Letter -> a
+letterToUnicode :: Letter -> Text
 letterToUnicode ltr =
-  let baseText = Textual.singleton (base ltr)
+  let baseText = Text.singleton (base ltr)
   in if macron ltr == Macron
-     then Textual.cons '_' baseText
+     then Text.cons '_' baseText
      else baseText
 
 -- | Translates a 'Word' to input syntax.
-wordToUnicode :: Textual a => Word -> a
+wordToUnicode :: Word -> Text
 wordToUnicode wd =
-  Textual.concat (map letterToUnicode (letters wd))
+  Text.concat (map letterToUnicode (letters wd))
 
 -- | Translates a 'Letter' to LaTeX syntax.
-letterToLaTeX :: Textual a => Letter -> a
+letterToLaTeX :: Letter -> Text
 letterToLaTeX ltr =
-  let baseText = Textual.singleton (base ltr)
+  let baseText = Text.singleton (base ltr)
   in if macron ltr == Macron
-     then Textual.concat [ Textual.fromString "\\_{"
-                         , baseText
-                         , Textual.singleton '}' ]
+     then Text.concat [ Text.fromString "\\_{"
+                      , baseText
+                      , Text.singleton '}' ]
      else baseText
 
 wordToLaTeX wd =
-  Textual.concat (map letterToLaTeX (letters wd))
+  Text.concat (map letterToLaTeX (letters wd))
